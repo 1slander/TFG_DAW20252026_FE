3a983b8dd213b657906cd466512b5c689cd72021
Alvaro Postigo <alvaro.postigo440@comunidadunir.net>
2026-01-14 18:54:07 +0100

    feat: add reusable dynamic form component feat: add form configurations for owner, employee and login feat: add register and login pages using dynamic form style: update form layout and fixed a typo in color scheme

---
+++ src/app/forms/login-form.ts
@@ -0,0 +1,6 @@
+import { FormField } from '../interfaces/form-field';
+
+export const LOGIN_Form:FormField[]=[
+    {name:'email',label:'Email',type:'email',required:true},
+    {name:'password',label:'Password',type:'password',required:true}
+];
---
+++ src/app/forms/owner-register.ts
@@ -0,0 +1,15 @@
+import { FormField } from '../interfaces/form-field';
+
+export const OWNER_REGISTER_FORM: FormField[] = [
+
+  // USERS
+  { name: 'first-name', label: 'Nombre', type: 'text', required: true },
+  { name: 'last-name', label: 'Apellidos', type: 'text', required: true },
+  { name: 'email', label: 'Email', type: 'email', required: true },
+
+  // RESTAURANT
+  { name: 'restaurant-name', label: 'Nombre del restaurante', type: 'text', required: true },
+  { name: 'address', label: 'Dirección', type: 'text' },
+  { name: 'phone', label: 'Teléfono', type: 'text' },
+  { name: 'capacity', label: 'Capacidad', type: 'number' }
+];
