1e883154b25f6d020398d6fc0c4c9c2b2a1c6b17
Alvaro Postigo <alvaro.postigo440@comunidadunir.net>
2026-01-28 11:12:07 +0100

    Refac: reestructuracion de las carpetas y los archivos del proyecto para mayor coherencia y mejor escalabilidad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { RegisterComponent } from './pages/register/register';
-import { LoginComponent } from './pages/login/login';
+import { RegisterComponent } from './features/auth/register/register';
+import { LoginComponent } from './features/auth/login/login';
 
 export const routes: Routes = [
   {
@@ -13,26 +13,26 @@ export const routes: Routes = [
   {
     path:'admin',
     loadChildren:()=>
-      import('./routes/admin.routes').then((m)=>m.adminRoutes)
+      import('./core/routes/admin.routes').then((m)=>m.adminRoutes)
   },
 
   //RUTAS PARA USER
   {
-    path: 'api',
+    path: 'user',
     loadChildren: () =>
-      import('./routes/api.routes').then((m) => m.apiRoutes),
+      import('./core/routes/user.routes').then((m) => m.userRoutes),
   },
   
   //RUTAS DE AUTH
  {
     path: 'auth',
     loadChildren: () =>
-      import('./routes/auth.routes').then((m) => m.authRoutes),
+      import('./core/routes/auth.routes').then((m) => m.authRoutes),
   },
 
   {
     path: '**',
-    redirectTo: 'api/dashboard',
+    redirectTo: 'user/dashboard',
   },
 ];
 
---
+++ src/app/shared/routes/dashboard.children.routes.ts
@@ -0,0 +1,39 @@
+import { Routes } from '@angular/router';
+import { Home } from '../../features/home/home';
+
+export const dashboardChildrenRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: 'home', component: Home, title: 'Home' },
+
+  {
+    path: 'settings',
+    loadChildren: () =>
+      import('../../features/settings/settings.routes').then(m => m.settingsRoutes),
+  },
+  {
+    path: 'editor',
+    loadChildren: () =>
+      import('../../features/editor/editor.routes').then(m => m.editorRoutes),
+  },
+  {
+    path: 'viewer',
+    loadChildren: () =>
+      import('../../features/viewer/viewer.routes').then(m => m.viewerRoutes),
+  },
+  {
+    path: 'car',
+    loadChildren: () =>
+      import('../../features/car/car.routes').then(m => m.carRoutes),
+  },
+  {
+    path: 'article',
+    loadChildren: () =>
+      import('../../features/article/article.routes').then(m => m.articleRoutes),
+  },
+  {
+    path: 'drawer',
+    loadChildren: () =>
+      import('../../features/drawer/drawer.routes').then(m => m.drawerRoutes),
+  },
+];
