e7c3f3f99affd510403bdecf6318ec4cbe88f3c9
Alvaro Postigo <alvaro.postigo440@comunidadunir.net>
2026-01-28 11:46:45 +0100

    refac: cambio de nombre para mayor legibilidad de dashboard-sections a sidebar-sections

--- src/app/shared/routes/dashboard.children.routes.ts
+++ src/app/shared/routes/dashboard.children.routes.ts
@@ -34,6 +34,6 @@ export const dashboardChildrenRoutes: Routes = [
   {
     path: 'drawer',
     loadChildren: () =>
-      import('../../features/dashboard-sections/drawer.routes').then(m => m.drawerRoutes),
+      import('../../features/sidebar-sections/drawer.routes').then(m => m.drawerRoutes),
   },
 ];
