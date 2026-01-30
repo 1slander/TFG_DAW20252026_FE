39485eb7cde76756fd345d1b4ffebdcb3f60ddd9
Alvaro Postigo <alvaro.postigo440@comunidadunir.net>
2026-01-30 17:28:01 +0100

    feat: creadas interfaces para la card usuarios fixed: problema con el padding en el formulario de creacion de admins fixed: cambio en el nombre app-drawer-menu por app-sidebar-menu

---
+++ src/app/interfaces/restaurant.ts
@@ -0,0 +1,11 @@
+export interface RestaurantInterface{
+    id:number;
+    cif:string;
+    name:string;
+    address:string;
+    country:string;
+    phone:string;
+    capacity:number;
+    totalTables:number;
+    owner: number;
+}
